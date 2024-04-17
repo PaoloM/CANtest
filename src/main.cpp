69c3e2faaa35d98d6312d62d9172db01fe628211
PaoloM <paolo@paolomarcucci.com>
2024-04-17 16:31:10 -0700

    First logger runs

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,10 @@ void setup()
 {
   Serial.begin(9600);
 
-  Serial.println("CAN Display - log recorder");
-  CAN0.setCANPins(GPIO_NUM_5, GPIO_NUM_4);
+  Serial.println("\n\nCAN Display - log recorder");
+  CAN0.setCANPins(GPIO_NUM_4, GPIO_NUM_5);
   CAN0.setListenOnlyMode(true);
-  CAN0.begin(500000);
+  CAN0.begin(CAN_BPS_500K);
   CAN0.watchFor();
 }
 
@@ -20,7 +20,8 @@ void loop()
   {
     Serial.print("CAN MSG: 0x");
     Serial.print(can_message.id, HEX);
-    Serial.print(" [");
+    Serial.print(" ");
+    Serial.print("[");
     Serial.print(can_message.length, DEC);
     Serial.print("] <");
     for (int i = 0; i < can_message.length; i++)
@@ -28,6 +29,7 @@ void loop()
       if (i != 0)
         Serial.print(":");
       Serial.print(can_message.data.byte[i], HEX);
+      Serial.print(" "); // maybe replace with \t for .tsv files
     }
     Serial.println(">");
   }
