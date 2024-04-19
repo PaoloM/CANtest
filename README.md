ad0db06c2c378dc082e364f7d9a237006334ffc5
PaoloM <paolo@paolomarcucci.com>
2024-04-19 13:33:09 -0700

    Streamlined the loggin format, increased the serial speed

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+https://github.com/P1kachu/talking-with-cars/blob/master/notes/fiat-500.txt
+https://github.com/fmntf/fiatcan
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,18 @@
 #include <Arduino.h>
 #include <esp32_can.h>
 
+const char separator = ',';
+
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
-  Serial.println("\n\nCAN Display - log recorder");
   CAN0.setCANPins(GPIO_NUM_4, GPIO_NUM_5);
   CAN0.setListenOnlyMode(true);
   CAN0.begin(CAN_BPS_500K);
   CAN0.watchFor();
+
+  Serial.println("\n\n---CAN logger ---");
 }
 
 void loop()
@@ -18,19 +21,14 @@ void loop()
 
   if (CAN0.read(can_message))
   {
-    Serial.print("CAN MSG: 0x");
-    Serial.print(can_message.id, HEX);
-    Serial.print(" ");
-    Serial.print("[");
-    Serial.print(can_message.length, DEC);
-    Serial.print("] <");
+    Serial.print(can_message.id, DEC);
+
     for (int i = 0; i < can_message.length; i++)
     {
-      if (i != 0)
-        Serial.print(":");
-      Serial.print(can_message.data.byte[i], HEX);
-      Serial.print(" "); // maybe replace with \t for .tsv files
+      Serial.print(separator);
+      Serial.print(can_message.data.byte[i], DEC);
     }
-    Serial.println(">");
+
+    Serial.println();
   }
 }
